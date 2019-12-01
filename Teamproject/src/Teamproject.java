8e999f34aa566b34f342ff59eb9d79221b491dc2
Tabea <tabea99@hotmail.de>
2019-12-01 18:26:48 +0100

    master pusher

--- Teamproject/src/Teamproject.java
+++ Teamproject/src/Teamproject.java
@@ -1,12 +1,12 @@
+import javafx.stage.DirectoryChooser;
 
 import java.io.File;
-
-import javafx.stage.DirectoryChooser;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBase;
@@ -15,68 +15,77 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 
+
+
 public class Teamproject extends Application {
-	//Bild Object
 	
-   	Image image;
+ 	Image image;
    	GridPane grid;
    	HBox UpperPane;
    	VBox BaseLayout;
    	
    	MenuBar menubar;
+   
    	
    	ImageView imageV;
    	
    	File d;
    	File[] fulllist;
    	
+   	ImageView [] imageArray = new ImageView[100];
+   	
+   	Button openButton;
+   	Button directoryButton;
+   	
    	int spalte=0;
    	int zeile=0;
+   	
+   	int Bildanzahl=0;
 
-	
 	public static void main(String[] args) {
-        launch(args);
-    }
-
-    private void addImagetoGrid(File f) {
+		launch(args);
+	}
+	
+	private void addImagetoGrid(File f) {
+	
 		//Image erstelln!!		
-		image = new Image("file:"+f.getPath());
-		
-		imageV = new ImageView();
+			image = new Image("file:"+f.getPath());
+			
+			imageV = new ImageView();
 
-		imageV.setFitHeight(200);
-        imageV.setPreserveRatio(true);
-		
-        imageV.setImage(image);
-        
-        grid.add(imageV, spalte, zeile);
-        spalte++;
+			imageV.setFitHeight(200);
+	        imageV.setPreserveRatio(true);
+			
+	        imageV.setImage(image);
+	        
+	        grid.add(imageV, spalte, zeile);
+	        spalte++;
 
-        if(spalte>2){
-        	
-        	zeile++;
-        	spalte=0;
-        }
-        	
-    }
-    
-    @Override
- 
-    public void start(Stage primaryStage) {
-    	    	
-       //Fenstertitel
-    	primaryStage.setTitle("Fotobuch");
+	        if(spalte>2){
+	        	
+	        	zeile++;
+	        	spalte=0;
+	        }
+	        
+	        	
+	    }
+	
+	    
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		
+		primaryStage.setTitle("Fotobuch");
     	
     	//Menubar
     	
@@ -97,47 +106,42 @@ public class Teamproject extends Application {
 
          BorderPane root = new BorderPane();
          root.setTop(menubar);
-    	
-    	
-    	//Panes
-    	//erste Ansicht
-    	
-    	BaseLayout = new VBox(5); 
-    	
-    	UpperPane = new HBox(2);
-    	
-        grid = new GridPane();
-        
-    	BaseLayout.getChildren().addAll(root,UpperPane,grid);
-    	
-    	
-    	Scene scene1 = new Scene(BaseLayout, 300, 300);
-    	
-    	
-    	//Bildraster
-    	//Gitter erstellen
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(10);
-        grid.setHgap(10);
-        
+         
+         BaseLayout = new VBox(5); 
+     	
+     	UpperPane = new HBox(2);
+     	
+         grid = new GridPane();
+         
+     	BaseLayout.getChildren().addAll(root,UpperPane,grid);
+     	
+     	
+     	Scene scene1 = new Scene(BaseLayout, 300, 300);
+     	
+     	
+     	//Bildraster
+     	//Gitter erstellen
+         grid.setPadding(new Insets(10, 10, 10, 10));
+         grid.setVgap(10);
+         grid.setHgap(10);
+         
+         
+       //FileChooser
+     	FileChooser chooser = new FileChooser();
+     	
+     	//Directory Chooser
+     	DirectoryChooser dChooser = new DirectoryChooser();
 
-                
-    	//FileChooser
-    	FileChooser chooser = new FileChooser();
-    	
-    	//Directory Chooser
-    	DirectoryChooser dChooser = new DirectoryChooser();
-
-    	
-    	//nur JPG-Dateien Zulassen
-    	chooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("All Images", "*.*"),
-                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
-                new FileChooser.ExtensionFilter("PNG", "*.png")
-            );
-    	
-    	//Knopf zum Öffnen
-        Button openButton = new Button("Bild auswählen");
+     	
+     	//nur JPG-Dateien Zulassen
+     	chooser.getExtensionFilters().addAll(
+                 new FileChooser.ExtensionFilter("All Images", "*.*"),
+                 new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                 new FileChooser.ExtensionFilter("PNG", "*.png")
+             );
+     	
+     	//Knopf zum Öffnen
+     	openButton = new Button("Bild auswählen");
 		UpperPane.getChildren().add(openButton);
         
         openButton.setOnAction(
@@ -157,10 +161,10 @@ public class Teamproject extends Application {
 
         });
         
-        Button directoryButton = new Button ("Bilderordner auswählen");
+        directoryButton = new Button ("Bilderordner auswählen");
         UpperPane.getChildren().add(directoryButton);
         
-        openDirectory.setOnAction(
+        directoryButton.setOnAction(
             	new EventHandler<ActionEvent>() {
             		
             		public void handle(ActionEvent e) {
@@ -173,6 +177,7 @@ public class Teamproject extends Application {
                 		
                 		for (int i=0; i<dlist.length;i++) {
                     		addImagetoGrid(dlist[i]);
+
                 		}
                 	}
             		
@@ -190,23 +195,16 @@ public class Teamproject extends Application {
             		
             	});
         
-        
-                
-         (new EventHandler<ActionEvent>() {
-            		
-            		public void handle(ActionEvent e) {
-            	     
-            			
-            			lol;
-             }
-            		
-            	});
-    	
- 
-        primaryStage.setScene(scene1);
-        primaryStage.show();
-    }
+        grid.addEventHandler(MouseEvent.MOUSE_CLICKED, e ->{
+            e.getX();
+            e.getY();
+           
+        });
+         
+         primaryStage.setScene(scene1);
+         primaryStage.show();
+     }
 
-}
+ }
 
 
